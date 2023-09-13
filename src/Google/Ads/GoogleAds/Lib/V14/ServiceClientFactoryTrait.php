cbcc29030c54490437d1f4405ef83551439d02d1
Sylvestre Ho <sylvestre.ho@alphalyr.com>
2023-09-13 17:20:44 +0200

    compatible with php 7.2

--- src/Google/Ads/GoogleAds/Lib/V14/ServiceClientFactoryTrait.php
+++ src/Google/Ads/GoogleAds/Lib/V14/ServiceClientFactoryTrait.php
@@ -203,10 +203,10 @@ trait ServiceClientFactoryTrait
                 $googleAdsLoggingInterceptor
             );
         }
-        array_push(
+        /*array_push(
             $clientOptions['transportConfig']['grpc']['interceptors'],
             ...$this->getGrpcInterceptors()
-        );
+        );*/
         if (!empty($this->getProxy())) {
             putenv('http_proxy=' . $this->getProxy());
         }
